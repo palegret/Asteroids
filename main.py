6a85b020202e05c8c0b631976818bd047653a298
Philip Alegret <philip.alegret@gmail.com>
2025-03-03 07:56:44 -0500

    Edited to use color string instead of tuple.

--- main.py
+++ main.py
@@ -10,14 +10,13 @@ def main():
     
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    black = (0, 0, 0)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-        screen.fill(color=black)
+        screen.fill("black")
         pygame.display.flip()
 
 
