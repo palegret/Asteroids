b72e99ce1f9ae29d41177875e78130ddbdada92b
Philip Alegret <philip.alegret@gmail.com>
2025-03-04 08:13:14 -0500

    Implemented updatable and drawable containers.

--- main.py
+++ main.py
@@ -5,9 +5,16 @@ from player import Player
 
 def main():
     pygame.init()
+    
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
     dt = 0
     
     while True:
@@ -15,9 +22,13 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
+        updatable.update(dt)
+
         screen.fill(SCREEN_COLOR)
-        player.update(dt)
-        player.draw(screen)
+
+        for obj in drawable:
+            obj.draw(screen)
+        
         pygame.display.flip()
 
         dt = clock.tick(GAME_FRAMERATE) / 1000
